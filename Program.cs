1e363e221728c6692fd1ea903e67c9190c75b9be
Sergey Aseev <serg046@outlook.com>
2022-12-20 19:24:56 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TryDotNetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
