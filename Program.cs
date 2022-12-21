fb8a90f8f303bbb12095238f93630fa6adc06347
Serg046 <serg046@outlook.com>
2022-12-22 01:23:16 +0300

    Push init version

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
-using System;
+using InterviewDotNet;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace TryDotNetDemo
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
