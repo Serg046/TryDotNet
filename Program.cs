14eabc1844c9dac2a8fcb2603b0af38c07a1ef3e
Serg046 <serg046@outlook.com>
2022-12-25 17:54:14 +0300

    Change the base tag

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using InterviewDotNet.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-const string baseTag = "/";
+const string baseTag = "/TryDotNet/";
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 var baseAddress = new Uri(new Uri(builder.HostEnvironment.BaseAddress), baseTag);
 builder.RootComponents.Add<ParameterBasedRouter>("#app");
