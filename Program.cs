546aff2f1cf17fcada62594dd26544aa289368fb
Serg046 <serg046@outlook.com>
2022-12-25 18:35:48 +0300

    Correct http paths

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 using InterviewDotNet.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-const string baseTag = "/TryDotNet/";
-Console.WriteLine(baseTag);
+const string baseTag = "/";
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 var baseAddress = new Uri(new Uri(builder.HostEnvironment.BaseAddress), baseTag);
 builder.RootComponents.Add<ParameterBasedRouter>("#app");
