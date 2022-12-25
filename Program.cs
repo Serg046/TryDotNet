5e12f0e1dc77fb21f5721cf9d88485edeafa05ac
Serg046 <serg046@outlook.com>
2022-12-25 17:35:37 +0300

    Correct the injected http client

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 using InterviewDotNet.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+const string baseTag = "/";
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var baseAddress = new Uri(new Uri(builder.HostEnvironment.BaseAddress), baseTag);
 builder.RootComponents.Add<ParameterBasedRouter>("#app");
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = baseAddress });
 await builder.Build().RunAsync();
